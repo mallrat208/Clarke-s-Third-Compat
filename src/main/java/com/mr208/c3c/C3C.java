88d52092cffae9d8dcf0558164976f2856660507
Andrew W <marwat208@gmail.com>
2017-03-02 02:27:00 -0500

    Initial Commit

---
+++ src/main/java/com/mr208/c3c/C3C.java
@@ -0,0 +1,13 @@
+package com.mr208.c3c;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = C3C.MOD_ID, name = C3C.MOD_NAME, version = C3C.MOD_VERSION, dependencies = C3C.MOD_DEPENDENCIES)
+public class C3C {
+
+	public final static String MOD_ID = "c3c";
+	public final static String MOD_NAME = "Clarke's Third Compat";
+	public final static String MOD_VERSION = "@VERSION@";
+	public final static String MOD_DEPENDENCIES = "required-after:Mekanism;required-after:base";
+
+}
