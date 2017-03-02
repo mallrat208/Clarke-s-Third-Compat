1aab130394413ec88b4e00befa780b9a643ff321
Andrew W <marwat208@gmail.com>
2017-03-02 05:19:15 -0500

    More work on Mekanism IMC

--- src/main/java/com/mr208/c3c/integration/mekanism/MekanismHelper.java
+++ src/main/java/com/mr208/c3c/integration/mekanism/MekanismHelper.java
@@ -5,10 +5,12 @@ import javax.annotation.Nonnull;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
+import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 
 import mekanism.api.gas.GasStack;
+import mekanism.api.gas.GasTank;
 
 public class MekanismHelper {
 
@@ -56,31 +58,78 @@ public class MekanismHelper {
 
 	}
 
-	public static void addElectrolyticSeperatorRecipe() {
+	public static void addElectrolyticSeperatorRecipe(@Nonnull GasStack leftOutput, @Nonnull GasStack rightOutput, @Nonnull FluidStack fluidInput, double energy, boolean deleteRecipe) {
+
+		NBTTagCompound recipeTag = new NBTTagCompound();
+		recipeTag.setTag("fluidInput", fluidInput.writeToNBT(new NBTTagCompound()));
+		recipeTag.setDouble("energy", energy);
+		recipeTag.setTag("leftOutput", leftOutput.write(new NBTTagCompound()));
+		recipeTag.setTag("rightOutput", rightOutput.write(new NBTTagCompound()));
+
+		sendIMC("ElectrolyticSeperatorRecipe", recipeTag, deleteRecipe);
+	}
+
+	public static void addPrecisionSawmillRecipe(@Nonnull ItemStack primaryOutput, @Nonnull ItemStack itemInput, boolean deleteRecipe) {
+
+		NBTTagCompound recipeTag = new NBTTagCompound();
+		recipeTag.setTag("itemInput", itemInput.writeToNBT(new NBTTagCompound()));
+		recipeTag.setTag("primaryOutput", primaryOutput.writeToNBT(new NBTTagCompound()));
+
+		sendIMC("PrecisionSawmillRecipe", recipeTag, deleteRecipe);
 
 	}
 
-	public static void addPrecisionSawmillRecipe() {
+	public static void addPrecisionSawmillRecipe(@Nonnull ItemStack primaryOutput,@Nonnull ItemStack secondaryOutput, double secondaryChance, @Nonnull ItemStack itemInput, boolean deleteRecipe) {
+
+		NBTTagCompound recipeTag = new NBTTagCompound();
+		recipeTag.setTag("itemInput", itemInput.writeToNBT(new NBTTagCompound()));
+		recipeTag.setTag("primaryOutput", primaryOutput.writeToNBT(new NBTTagCompound()));
+		recipeTag.setTag("seconadryOutput", secondaryOutput.writeToNBT(new NBTTagCompound()));
+		recipeTag.setDouble("secondaryChance", secondaryChance);
+
+		sendIMC("PrecisionSawmillRecipe", recipeTag, deleteRecipe);
 
 	}
 
-	public static void addChemicalDissolutionChamberRecipe() {
+	public static void addChemicalDissolutionChamberRecipe(@Nonnull GasStack gasOutput, @Nonnull ItemStack itemInput, boolean deleteRecipe) {
+
+		NBTTagCompound recipeTag = new NBTTagCompound();
+		recipeTag.setTag("itemInput",itemInput.writeToNBT(new NBTTagCompound()));
+		recipeTag.setTag("gasOutput", gasOutput.write(new NBTTagCompound()));
+
+		sendIMC("ChemicalDissolutionChamberRecipe", recipeTag, deleteRecipe);
 
 	}
 
-	public static void addChemicalWasherRecipe() {
+	public static void addChemicalWasherRecipe(@Nonnull GasStack gasOutput, @Nonnull GasStack gasInput, boolean deleteRecipe) {
+
+		NBTTagCompound recipeTag = new NBTTagCompound();
+		recipeTag.setTag("gasInput", gasInput.write(new NBTTagCompound()));
+		recipeTag.setTag("gasOutput", gasOutput.write(new NBTTagCompound()));
+
+		sendIMC("ChemicalWasherRecipe", recipeTag, deleteRecipe);
+	}
+
+	public static void addChemicalCrystallizerRecipe(@Nonnull ItemStack itemOutput, @Nonnull GasStack gasInput, boolean deleteRecipe) {
+
+		NBTTagCompound recipeTag = new NBTTagCompound();
+		recipeTag.setTag("gasInput", gasInput.write(new NBTTagCompound()));
+		recipeTag.setTag("itemOutput", itemOutput.writeToNBT(new NBTTagCompound()));
+
+		sendIMC("ChemicalCrystallizerRecipe", recipeTag, deleteRecipe);
+	}
+
+	public static void modifySolarNeutronActivatorRecipe(@Nonnull GasStack gasOutput, @Nonnull GasStack gasInput, boolean deleteRecipe) {
+
+		NBTTagCompound recipeTag = new NBTTagCompound();
+		recipeTag.setTag("gasInput", gasInput.write(new NBTTagCompound()));
+		recipeTag.setTag("gasOutput", gasOutput.write(new NBTTagCompound()));
+
+		sendIMC("SolarNeutronActivatorRecipe", recipeTag, deleteRecipe);
 
 	}
 
-	public static void addChemicalCrystallizerRecipe() {
-
-	}
-
-	public static void addSolarNeutronActivatorRecipe() {
-
-	}
-
-	public static void addPressurizedReactionChamberRecipe(@Nonnull ItemStack itemOutput, @Nonnull GasStack gasOutput,@Nonnull ItemStack itemInput,@Nonnull FluidStack fluidInput,@Nonnull GasStack gasInput) {
+	public static void modifyPressurizedReactionChamberRecipe(@Nonnull ItemStack itemOutput, @Nonnull GasStack gasOutput,@Nonnull ItemStack itemInput,@Nonnull FluidStack fluidInput,@Nonnull GasStack gasInput,boolean deleteRecipe) {
 
 		NBTTagCompound recipeTag = new NBTTagCompound();
 		recipeTag.setTag("itemInput", itemInput.writeToNBT(new NBTTagCompound()));
@@ -90,11 +139,32 @@ public class MekanismHelper {
 		recipeTag.setTag("itemOutput", itemOutput.writeToNBT(new NBTTagCompound()));
 		recipeTag.setTag("gasOutput", gasOutput.write(new NBTTagCompound()));
 
-		sendIMC("PressurizedReactionChamberRecipe", recipeTag);
+		sendIMC("PressurizedReactionChamberRecipe", recipeTag,deleteRecipe);
 	}
 
-	protected static void sendIMC(String messageType, NBTTagCompound recipeTag)
+	protected static void sendIMC(String messageType, NBTTagCompound recipeTag, boolean deleteRecipe)
 	{
-		FMLInterModComms.sendMessage("Mekanims", messageType, recipeTag);
+		FMLInterModComms.sendMessage("Mekanims", (deleteRecipe?"Delete":"")+messageType, recipeTag);
+	}
+
+	public enum InfuseType {
+		CARBON("CARBON"),
+		TIN("TIN"),
+		DIAMOND("DIAMOND"),
+		REDSTONE("REDSTONE"),
+		FUNGI("FUNGI"),
+		BIO("BIO"),
+		OBSIDIAN("OBSIDIAN");
+
+		private String name;
+
+		String getName(){
+			return this.name;
+		}
+
+		InfuseType(String n){
+
+			name = n;
+		}
 	}
 }
